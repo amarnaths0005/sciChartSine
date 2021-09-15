5873d63df2bde6a325b44a8716edb1311cfd55cd
Amarnath S <45934217+amarnaths0005@users.noreply.github.com>
2021-09-15 09:57:42 +0530

    Simple WPF MVVM Sine Curve Application
    Simple WPF MVVM application to display a Sine Curve. 
    - Model generates the Sine Curve, and exposes the points generated
    - ViewModel reads the point list generated by the model, and exposes it to the View
    - View consumes the point list, and other view-related strings exposed by the ViewModel
    - Properties contains a resource file having a list of all strings displayed in the View

---
+++ Model/SineCurve.cs
@@ -0,0 +1,60 @@
+﻿using SciChart.Charting.Model.DataSeries;
+using System;
+using SciChartSine.Properties;
+
+
+namespace SciChartSine.Model
+{
+    public class SineCurve
+    {
+        private double _phase;
+        private double _amplitude;
+        public SineCurve()
+        {
+            LineData = new XyDataSeries<double, double>() { SeriesName = Strings.SineCurve_SeriesName };
+            UpdateCurve();
+        }
+
+        public void UpdateCurve()
+        {
+            GenerateRandomAmplitudeAndPhase();
+            GenerateSineData();
+        }
+
+        public XyDataSeries<double, double> LineData { get; private set; }
+
+        #region HelperFunctions
+        private void GenerateRandomAmplitudeAndPhase()
+        {
+            double minPhase = 0; // degrees
+            double maxPhase = 180; // degrees
+            double minAmplitude = 2;
+            double maxAmplitude = 50;
+
+            Random random = new Random();
+            double ampRand = random.NextDouble();
+            _amplitude = minAmplitude + ampRand * (maxAmplitude - minAmplitude);
+
+            double phaseRand = random.NextDouble();
+            _phase = minPhase + phaseRand * (maxPhase - minPhase); // Degrees
+            _phase *= Math.PI / 180.0; // Radians
+        }
+
+        private void GenerateSineData()
+        {
+            int noDivisions = 200;
+            double totalRange = 4 * Math.PI;
+            double step = totalRange / noDivisions;
+
+            LineData.Clear();
+
+            for (int i = 0; i <= noDivisions; ++i)
+            {
+                double x = i * step;
+                double y = _amplitude * Math.Sin(x + _phase);
+                LineData.Append(x, y);
+            }
+        }
+        #endregion
+    }
+}
---
+++ ViewModel/SineCurveViewModel.cs
@@ -0,0 +1,125 @@
+﻿using SciChart.Charting.Model.ChartSeries;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Data.Model;
+using SciChartSine.Model;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+using SciChartSine.Properties;
+
+namespace SciChartSine.ViewModel
+{
+    public class SineCurveViewModel : BindableObject
+    {
+        private string _chartTitle = Strings.SineCurve_ChartTitle;
+        private string _xAxisTitle = Strings.SineCurve_XaxisTitle;
+        private string _yAxisTitle = Strings.SineCurve_YaxisTitle;
+        private string _bnGenerateNew = Strings.SineCurve_Button_GenerateNew;
+        private string _windowTitle = Strings.SineCurve_WindowTitle;
+        private SineCurve _sine;
+        private XyDataSeries<double, double> _sineData;
+        private RelayCommand _generateCurveCommand;
+
+        private ObservableCollection<IRenderableSeriesViewModel> _renderableSeries;
+
+        public SineCurveViewModel()
+        {
+            _renderableSeries = new ObservableCollection<IRenderableSeriesViewModel>();
+            _sine = new SineCurve();
+            _sineData = new XyDataSeries<double, double>();
+            GenerateNewCurve();
+        }
+
+        public ObservableCollection<IRenderableSeriesViewModel> RenderableSeries
+        {
+            get { return _renderableSeries; }
+            set { _renderableSeries = value;
+                OnPropertyChanged(nameof(RenderableSeries));
+            }
+        }
+        public string ChartTitle
+        {
+            get { return _chartTitle; }
+            set
+            {
+                _chartTitle = value;
+                OnPropertyChanged(nameof(ChartTitle));
+            }
+        }
+        public string XAxisTitle
+        {
+            get { return _xAxisTitle; }
+            set
+            {
+                _xAxisTitle = value;
+                OnPropertyChanged(nameof(XAxisTitle));
+            }
+        }
+        public string YAxisTitle
+        {
+            get { return _yAxisTitle; }
+            set
+            {
+                _yAxisTitle = value;
+                OnPropertyChanged(nameof(YAxisTitle));
+            }
+        }
+
+        public string BnGenerateNew
+        {
+            get { return _bnGenerateNew; }
+            set
+            {
+                _bnGenerateNew = value;
+                OnPropertyChanged(nameof(BnGenerateNew));
+            }
+        }
+
+        public string WindowTitle
+        {
+            get { return _windowTitle; }
+            set
+            {
+                _windowTitle = value;
+                OnPropertyChanged(nameof(WindowTitle));
+            }
+        }
+
+        public void GenerateNewCurve()
+        {
+            _sine.UpdateCurve();
+            _sineData = _sine.LineData;
+            _sineData.InvalidateParentSurface(RangeMode.ZoomToFit);
+
+            RenderableSeries.Clear();
+            RenderableSeries.Add(new LineRenderableSeriesViewModel()
+            {
+                StrokeThickness = 2,
+                Stroke = Colors.Azure,
+                DataSeries = _sineData,
+            }
+                );
+        }
+
+        bool CanGenerate
+        {
+            get { return true; }
+        }
+
+        public RelayCommand GenerateCurveCommand
+        {
+            get
+            {
+                if (_generateCurveCommand == null)
+                {
+                    _generateCurveCommand = new RelayCommand(
+                        param => GenerateNewCurve(),
+                        param => CanGenerate
+                        );
+                }
+                return _generateCurveCommand;
+            }
+            set => _generateCurveCommand = value;
+        }
+
+    }
+}
